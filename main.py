3b7e723ade8dc4ccece2157e1be6811230222ca9
PiotrKaluzny <15984108+khashishin@users.noreply.github.com>
2022-05-11 18:51:33 +0200

    Czytelniejsza wersja z funkcjami

--- main.py
+++ main.py
@@ -3,24 +3,27 @@ figury=[]
 liczba_figur=int(float(input().strip()))
 for i in range(liczba_figur):
   figury.append(input().split())
-# print(figury)
+
+def pole_prost(a,b):
+  return a*b
+
+def pole_kol(r):
+  return math.pi * (r)**2
+
+def pole_trojk(a,b,c):
+  p = ((float(a))+(float(b))+(float(c)))/2
+  return math.sqrt(p*(p-a)*(p-b)*(p-c))
+
 pole=0
 for figura in figury:
-  if len(figura[0:])>3:
+  if len(figura)>3:
     print("Błąd: można podać maksymalnie 3 liczby") 
     break
-  elif len(figura[0:])==1:
-    wzor_kola=math.pi*(float(figura[0]))**2
-    pole+=wzor_kola
-  elif len(figura[0:])==2:
-    wzor_prostokat=(float(figura[0]))*(float(figura[1]))
-    pole += wzor_prostokat
-  elif len(figura[0:])==3:
-    p=((float(figura[0]))+(float(figura[1]))+(float(figura[2])))/2
-    a=(float(figura[0]))
-    b=(float(figura[1]))
-    c=(float(figura[2]))
-    wzor_trojkat=math.sqrt(p*(p-a)*(p-b)*(p-c))
-    pole += wzor_trojkat
-else:
-  print(round(pole,2))
+  elif len(figura)==1:
+    pole += pole_kol(figura[0])
+  elif len(figura)==2:
+    pole += pole_prost(figura[0],figura[1])
+  elif len(figura)==3:
+    pole += pole_trojk(figura[0],figura[1],figura[2])
+    
+print(round(pole,2))
